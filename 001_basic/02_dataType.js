00562491a6416347af5ae62f32edf6819070e43e
Zeba shafi <136117753+zebashafi2110@users.noreply.github.com>
2024-09-12 15:43:49 +0000

    dataType.js

---
+++ 001_basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//  alert (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("zeba")
+
+let name = "zeba"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bignit
+// String =>""
+//  boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
