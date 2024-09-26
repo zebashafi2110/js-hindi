6201489b7bf0470e5cdce47fca2097e7349bdb84
Zeba shafi <136117753+zebashafi2110@users.noreply.github.com>
2024-09-26 09:26:06 +0000

    container config done

---
+++ 002_basic/03_objects.js
@@ -0,0 +1,17 @@
+//  singleton
+// object.create
+
+// objects literals
+const JsUser = {
+    name: "zeba",
+    
+    age: 24,
+    location: "Delhi",
+    email: "zebashafi@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Satuerday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
