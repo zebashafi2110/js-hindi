9de9b65407faad8652f36e074861dc064b42747f
Zeba shafi <136117753+zebashafi2110@users.noreply.github.com>
2024-09-28 15:04:18 +0000

    container config done

--- 002_basic/03_objects.js
+++ 002_basic/03_objects.js
@@ -1,17 +1,43 @@
 //  singleton
-// object.create
+// object.create this is called constructor method.....
 
 // objects literals
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "zeba",
     
     age: 24,
     location: "Delhi",
     email: "zebashafi@google.com",
+    [mySym] : "myKey1", // here i used [] breket cus of i want refrence of symbol 
     isLoggedIn: false,
     lastLoginDays: ["Monday","Satuerday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]);
 
+JsUser.email = "zebashafi@chatgpt.com"
+// Object.freeze(JsUser)
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("hello JS user");
+    
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`hello JS user,${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+
+console.log(JsUser.greetingTwo());
+
+// object literal*****************
---
+++ 002_basic/04_objects.js
@@ -0,0 +1,41 @@
+// singleton object
+//  const tinderUser = new object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Alll"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email : "some@gmail.com",
+    fullname:{
+        firstname : "zeba",
+        lastname : "shafi"
+    }
+}
+
+// console.log(regularUser.fullname.lastname);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+//  const obj3 = {obj1, obj2}
+//  const obj3 = Object.assign({}, obj1, obj2,obj4)
+//  console.log(obj3);
+ 
+//  const obj3 = {...obj1, ...obj2}
+//  console.log(obj3);
+ 
+// user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
